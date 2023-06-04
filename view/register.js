70a22e4d2249e2b885007787b78c312e2d23bbce
HongHanHannah <109467736+HongHanHannah@users.noreply.github.com>
2023-06-04 21:40:04 +0700

    Spck

--- view/register.js
+++ view/register.js
@@ -112,7 +112,7 @@ createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    // ...
+    window.location.href = "./index.html"
   })
   .catch((error) => {
     const errorCode = error.code;
